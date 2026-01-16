57b0569ed2e0db4318a5dfa6e1624f00620c9a18
aswanivaChaithram <aswaniva040@gmail.com>
2026-01-16 19:35:25 +0530

    test - react app

---
+++ MetaSciforTests/ReactTest/test19/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <h2>LOGO</h2>
+        <div className="navbar-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
